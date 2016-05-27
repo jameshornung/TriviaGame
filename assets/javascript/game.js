037292ce1f11a933fd6fd14f07e2cd344bd39b23
James Hornung <hornung.james@gmail.com>
2016-05-27 12:54:32 -0500

    basic functions of game working, ten questions added, audio added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,212 @@
 //Global Variables
 //------------------------------------------------------------------------------------------------
-questionNum = 1;
 answerCorrect = 0;
 incorrectAnswer =0;
 notAnswered = 0;
 x = 0;
-noAnswer == true;
+var groovy = document.getElementById("groove");
+var imagine = document.getElementById("dreamer");
 
 var questions = {
 	quest: [
 	{
 		question: "this is question 1",
 		correctAnswer: "this is the correct answer",
-		wrongAnswer: "this is a wrong answer",
-		wrongAnswer2: "this is another wrong answer",
-		wrongAnswer3: "this is the final wrong answer"
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
 	},
 	{
-		question: "this is question 2",
-		correctAnswer: "this is the correct answer",
-		wrongAnswer: "this is a wrong answer",
-		wrongAnswer2: "this is another wrong answer",
-		wrongAnswer3: "this is the final wrong answer"
+		question: "which uses the most electricity?",
+		correctAnswer: "cowboys stadium",
+		answers: [
+			"an electric car",
+			"cowboys stadium",
+			"lighting the UT tower",
+			"liberia"
+			],
 	},
 	{
 		question: "this is question 3",
 		correctAnswer: "this is the correct answer",
-		wrongAnswer: "this is a wrong answer",
-		wrongAnswer2: "this is another wrong answer",
-		wrongAnswer3: "this is the final wrong answer"
-	},]
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},
+	{
+		question: "this is question 4",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},
+	{
+		question: "the gdp of texas is roughly equivalent to that of which country?",
+		correctAnswer: "mexico",
+		answers: [
+			"mexico",
+			"russia",
+			"uzbekistan",
+			"kenya"
+			],
+	},
+	{
+		question: "this is question 6",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},
+	{
+		question: "this is question 7",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},		
+	{
+		question: "this is question 8",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},
+	{
+		question: "this is question 9",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			],
+	},
+	{
+		question: "this is question 10",
+		correctAnswer: "this is the correct answer",
+		answers: [
+			"fjdlfkjd",
+			"fddsg",
+			"ffsdfds",
+			"this is the correct answer"
+			]
+	},
+	],
 }
 
 
-console.log(questions.quest[x]);
-// $("#questionBox").html(questions.quest[x].question);
 
 //Functions
 //------------------------------------------------------------------------------------------------
-function showQuestion()
-{
-	setInterval(function(){
-	$("#questionBox").html(questions.quest[x].question + "<br>");
-	$("#questionBox").append(questions.quest[x].wrongAnswer + "<br>");
-	$("#questionBox").append(questions.quest[x].correctAnswer + "<br>");
-	$("#questionBox").append(questions.quest[x].wrongAnswer2 + "<br>");
-	$("#questionBox").append(questions.quest[x].wrongAnswer3 + "<br>");
-	x++;
-}, 2000)
-};
+
+function display(){
+	$("#title").html(questions.quest[x].question);
+	$("#firstAnswer").html(questions.quest[x].answers[0]);
+	$("#secondAnswer").html(questions.quest[x].answers[1]);
+	$("#thirdAnswer").html(questions.quest[x].answers[2]);
+	$("#fourthAnswer").html(questions.quest[x].answers[3]);
+}
+
+function playBeasties(){
+	groovy.play();
+}
+
+function stopBeasties(){
+	groovy.pause();
+}
+
+function playEddie(){
+	imagine.play();
+}
+
+function playGame(){
+	console.log(x);
+
+		if(x === 0)
+		{
+			
+		};
+		
+		if(x === 1)
+		{
+			display();
+		};
+		
+		if(x === 2)
+		{
+			display();
+			
+		};
+		
+		if(x === 3)
+		{
+			display();
+			
+		};
+
+		if(x === 4)
+		{
+			display();
+		};
+
+		if(x === 5)
+		{
+			display();
+		};
+
+		if(x === 6)
+		{
+			display();
+		};
+
+		if(x === 7)
+		{
+			display();
+		};
+
+		if(x === 8)
+		{
+			display();
+		};
+
+		if(x === 9)
+		{
+			display();
+		};
+	
+		if(x === 10)
+		{
+			stopBeasties();
+			playEddie();
+			notAnswered = 10 - (answerCorrect + incorrectAnswer);
+			$("#title").html("Game Over");
+			$("#firstAnswer").html("Correct: " + answerCorrect);
+			$("#secondAnswer").html("Incorrect: " + incorrectAnswer);
+			$("#thirdAnswer").html("Not Answered: " + notAnswered);
+			$("#fourthAnswer").html("");
+			//reset button
+		}
+
+		x++;
+}
 
 
 
@@ -56,17 +214,52 @@ function showQuestion()
 //General Code
 //------------------------------------------------------------------------------------------------
 
-$("startButton").click(function(e){
+$("#startButton").click(function(e)
+{	
+	$("#startButton").hide();
+	display();	
+	playBeasties();
+	var timer = setInterval(function() {playGame()}, 10000);
 	
-	for(i=0; i<3; i++){
-		showQuestion();
-		//if answer is correct
 
-		//if answer is incorrect
+$(".answer").click(function(e){
+	console.log(this.textContent);
+			if (this.textContent === questions.quest[x].correctAnswer)
+			{
+				$("#title").html("Correct!!!");
+				$("#firstAnswer").html("");
+				$("#secondAnswer").html("");
+				$("#thirdAnswer").html("");
+				$("#fourthAnswer").html("");
+				answerCorrect = answerCorrect + 1;
+			}
+			else
+			{
+				$("#title").html("Wrong Answer");
+				$("#firstAnswer").html("");
+				$("#secondAnswer").html("");
+				$("#thirdAnswer").html("");
+				$("#fourthAnswer").html("");
+				incorrectAnswer = incorrectAnswer + 1;
+			}
 
-		//if no answer serlected
-		if(noAnswer){
+	});	
+
 			
-		}
-	}
-})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
